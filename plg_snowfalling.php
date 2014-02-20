361c36dd4d83762b4da6f53a7553f31723267c8d
Clemens Neubauer <cn-tools@gmx.at>
2014-02-20 18:43:57 +0100

    no flakes output
    because the update service was integrated there was no snowflakes
    displaying

--- plg_snowfalling.php
+++ plg_snowfalling.php
@@ -23,9 +23,9 @@
  */
 defined('_JEXEC') or die('Restricted access');
 
-class plgSystemSnowFalling extends JPlugin
+class plgSystemPlg_SnowFalling extends JPlugin
 {
-	function plgSystemSnowFalling(&$subject, $config)
+	function plgSystemPlg_SnowFalling(&$subject, $config)
 	{
 		parent::__construct($subject, $config);
 	}
@@ -75,7 +75,7 @@ class plgSystemSnowFalling extends JPlugin
 			$pos = strrpos($buffer, "</head>");
 			if($pos > 0)
 			{
-				$buffer = substr($buffer, 0, $pos).'<script src="'.JURI::base().'plugins/system/snowfalling/snow.js" type="text/javascript"></script>'.'<script type="text/javascript">'.$script.'</script>'.substr($buffer, $pos);
+				$buffer = substr($buffer, 0, $pos).'<script src="'.JURI::base().'plugins/system/plg_snowfalling/snow.js" type="text/javascript"></script>'.'<script type="text/javascript">'.$script.'</script>'.substr($buffer, $pos);
 			}		
 			
 			JResponse::setBody($buffer);
